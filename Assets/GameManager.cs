58f4836c204764e4e3da25c879f561cab984cf2c
umkcVR <137325046+umkcVR@users.noreply.github.com>
2023-06-28 21:03:27 -0500

    Added object spawner

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public List<GameObject> objects;
+    public float radius = 5f, height = 10.0f; // The radius of the circular area
+    
+    private void Start()
+    {
+        StartCoroutine("SpawnObject");
+    }
+
+    private IEnumerator SpawnObject()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SpawnObjectsInCircularArea();
+        StartCoroutine("SpawnObject");
+    }
+
+    public void SpawnObjectsInCircularArea()
+    {
+        float angle = Random.Range(0, 360); // Calculate the angle for each object
+        float radiusRandomized = radius * (UnityEngine.Random.Range(0, 10));
+        Vector3 spawnPosition = transform.position + Quaternion.Euler(0f, angle, 0f) * (Vector3.forward * radius) + (Vector3.up * height);
+        Instantiate(objects[Random.Range(0, objects.Count)], spawnPosition, Quaternion.identity, gameObject.transform);
+    }
+}
--- Assets/SpawnerScript.cs
+++ Assets/SpawnerScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Enemy;
+    public float Timer = 0;
+    public float SpawnFrequency = 10;
+    public float MaxEnemies = 5;
+    public float CurrentEnemies = 0;
+    public float SpawnRad = 50;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Spawn();
+        Timer = Timer + Time.deltaTime;
+    }
+
+    void Spawn()
+    {
+        if (Timer >= SpawnFrequency && CurrentEnemies < MaxEnemies)
+        {
+            Instantiate(Enemy, new Vector3(Random.Range(-SpawnRad, SpawnRad), transform.position.y,Random.Range(-SpawnRad, SpawnRad)), transform.rotation);
+            Debug.Log("Enemy Spawned");
+            Timer = 0;
+        }
     }
 }
